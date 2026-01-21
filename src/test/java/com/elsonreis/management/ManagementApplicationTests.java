c4d9ee486bdd91c29c0a51a837d0f94271643992
elsonreis77 <elsonreis084@gmail.com>
2026-01-21 18:39:30 -0300

    chore: initial Spring Boot project setup

---
+++ src/test/java/com/elsonreis/management/ManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsonreis.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
