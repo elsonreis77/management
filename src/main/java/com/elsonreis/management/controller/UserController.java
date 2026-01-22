c220b0c799f2e5566941f9901f299c9610e3f1d5
elsonreis77 <elsonreis084@gmail.com>
2026-01-22 20:09:21 -0300

    feat: add user controller

--- src/main/java/com/elsonreis/management/controller/UserController.java
+++ src/main/java/com/elsonreis/management/controller/UserController.java
@@ -1,12 +1,44 @@
 package com.elsonreis.management.controller;
 
 import com.elsonreis.management.entity.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.elsonreis.management.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private final
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+
+        User createUser = userService.createuser(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<java.util.List<User>> getAllUsers() {
+
+        List<User> users = userService.getAllUsers();
+
+        return ResponseEntity.ok(users);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+
+        userService.deleteUser(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
