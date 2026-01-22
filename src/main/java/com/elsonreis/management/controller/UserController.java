c412e7cf189c7052b2b9dc49f917f9e171fd9e5b
elsonreis77 <elsonreis084@gmail.com>
2026-01-22 18:51:18 -0300

    feat: add user controller

---
+++ src/main/java/com/elsonreis/management/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.elsonreis.management.controller;
+
+import com.elsonreis.management.entity.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final 
+}
